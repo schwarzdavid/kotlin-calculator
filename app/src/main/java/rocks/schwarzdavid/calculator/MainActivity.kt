56da0f874ff6e37c8701356d6a07f67a95394434
David Schwarz <schwarz.david10@gmail.com>
2020-01-26 02:16:07 +0100

    Main Activity updated

--- app/src/main/java/rocks/schwarzdavid/calculator/MainActivity.kt
+++ app/src/main/java/rocks/schwarzdavid/calculator/MainActivity.kt
@@ -5,48 +5,121 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
-import rocks.schwarzdavid.calculator.helper.MathParser
+import kotlinx.android.synthetic.main.activity_main.*
+import io.kaen.dagger.ExpressionParser
+import java.lang.Exception
 
 const val STATE_INPUT_CONTENT = "inputContent"
 
 class MainActivity : AppCompatActivity() {
 
-    private var _inputTextView: TextView? = null;
-    private var _previewTextView: TextView? = null;
+    private val OPERATORS = setOf<Char>('*', '/', '+', '-');
+    private var _openBrackets = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        _inputTextView = findViewById<TextView>(R.id.inputText);
-        _previewTextView = findViewById<TextView>(R.id.previewText);
+        val rootControlGroup = findViewById<ViewGroup>(R.id.controls);
+
+        for (numBtn in findViewsByTag(rootControlGroup, getString(R.string.type_number))) {
+            numBtn.setOnClickListener {
+                appendToExpression((it as Button).text);
+            }
+        }
+
+        for (opBtn in findViewsByTag(rootControlGroup, getString(R.string.type_operator))) {
+            opBtn.setOnClickListener {
+                input_text.text = stripOperatorFromEnd(input_text.text);
+                appendToExpression((it as Button).text);
+            }
+        }
+
+        open_bracket.setOnClickListener {
+            _openBrackets++;
+            appendToExpression((it as Button).text);
+        }
+
+        close_bracket.setOnClickListener {
+            if (_openBrackets > 0) {
+                _openBrackets--;
+                appendToExpression((it as Button).text);
+            }
+        }
+
+        clear_one.setOnClickListener {
+            input_text.text = input_text.text.dropLast(1);
+
+            if (input_text.text.isEmpty()) {
+                input_text.text = getString(R.string.default_result);
+            }
+        }
+
+        clear_all.setOnClickListener {
+
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putString(STATE_INPUT_CONTENT, _inputTextView?.getText().toString());
+        outState.putString(STATE_INPUT_CONTENT, input_text.getText().toString());
 
         super.onSaveInstanceState(outState)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        _inputTextView?.text = savedInstanceState.getString(STATE_INPUT_CONTENT);
+        input_text.text = savedInstanceState.getString(STATE_INPUT_CONTENT);
 
         super.onRestoreInstanceState(savedInstanceState)
     }
 
+    /**
+     * @see https://stackoverflow.com/a/8831593
+     */
     fun findViewsByTag(root: ViewGroup, tag: String): ArrayList<View> {
         val views = arrayListOf<View>();
-        // TODO: https://stackoverflow.com/a/8831593
+        for (i in 0 until root.getChildCount()) {
+            val child = root.getChildAt(i);
+            if (child is ViewGroup) {
+                views.addAll(findViewsByTag(child, tag));
+            }
+
+            val tagObj = child.getTag();
+            if (tagObj !== null && tagObj.equals(tag)) {
+                views.add(child);
+            }
+        }
+        return views;
     }
 
-    fun onInputButtonClick(view: View) {
-        val text = (view as Button).text;
-        if (_inputTextView?.text === getString(R.string.default_input)) {
-            _inputTextView?.setText(text);
+    fun appendToExpression(txt: CharSequence) {
+        input_text.append(txt);
+
+        if (input_text.text.isEmpty()) {
+            preview_text.setText(getString(R.string.default_result));
         } else {
-            _inputTextView?.append(text);
+            preview_text.setText(
+                evaluateMathExpression(
+                    stripOperatorFromEnd(input_text.text).toString() + ")".repeat(
+                        _openBrackets
+                    )
+                ).toString()
+            );
         }
-        _inputTextView?.setAutoSizeTextTypeWithDefaults(TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM);
+    }
+
+    fun stripOperatorFromEnd(expression: CharSequence): CharSequence {
+        var out = expression;
+        if (OPERATORS.contains(out.takeLast(1).last())) {
+            out = out.dropLast(1);
+        }
+        return out;
+    }
+
+    fun evaluateMathExpression(expression: String): Double? {
+        try {
+            return ExpressionParser().evaluate(expression);
+        } catch (e: Exception) {
+            return null;
+        }
     }
 }
